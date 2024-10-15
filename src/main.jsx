f4a7d4b396afcb926e4ddc95b3da2b7aa534f323
Mdelmont <matthieu@delmont.fr>
2024-10-15 09:15:34 +0200

    test

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { GetProviderStore } from "./store/routerStore.jsx";
 import "./styles/main.scss"
 import "./styles/components/secondaryBtn.scss"
+// test déploiment
 ReactDOM.createRoot(document.getElementById('roothtml')).render(
   <React.StrictMode>
     <GetProviderStore>
