c62972b3f2ad349e4cef5754b0fdccb9be7c7ca1
Mdelmont <matthieu@delmont.fr>
2024-10-17 11:52:05 +0200

    make dashbord app

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -127,8 +127,10 @@ const  DashbordPage = ()  => {
                   <td>
                     <a href={`${BASE_URL}/register/${invitation.token}`}>{BASE_URL}/register/{invitation.token}</a>
                   </td>
-                  <td>
-                  <Modal btnUse={<BtnPrimary title={'Annuler l\'invitation'} />}>
+                  <td className="action-td">
+                  <Modal btnUse={
+                     <svg className="icon-btn"  viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                  }>
                       <div className="modal-cent-confirmation">
                         <h2>Etes vous sur de vouloir supprimer l'invitation ? </h2>
                         <BtnPrimary onClick={handleRemoveInvit} id={invitation.id} title="Confirmer la suppression"/>
@@ -164,14 +166,18 @@ const  DashbordPage = ()  => {
                   <td>{user.email}</td>
                   <td>{user.budget}</td>
                   <td className="action-td">
-                  <BtnPrimary onClick={handleUpdateGame} id={user.id} title="Modifier l'utilisateur"/>
-                    <Modal btnUse={<BtnPrimary title={"Supprimer l'utilisateur"} />}>
+                    <div className="icon-btn-cont">
+                  <svg className="icon-btn" onClick={handleUpdateGame}  id={user.id}  viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.8536 1.14645C11.6583 0.951184 11.3417 0.951184 11.1465 1.14645L3.71455 8.57836C3.62459 8.66832 3.55263 8.77461 3.50251 8.89155L2.04044 12.303C1.9599 12.491 2.00189 12.709 2.14646 12.8536C2.29103 12.9981 2.50905 13.0401 2.69697 12.9596L6.10847 11.4975C6.2254 11.4474 6.3317 11.3754 6.42166 11.2855L13.8536 3.85355C14.0488 3.65829 14.0488 3.34171 13.8536 3.14645L11.8536 1.14645ZM4.42166 9.28547L11.5 2.20711L12.7929 3.5L5.71455 10.5784L4.21924 11.2192L3.78081 10.7808L4.42166 9.28547Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                  {/* <BtnPrimary onClick={handleUpdateGame} id={user.id} title="Modifier l'utilisateur"/> */}
+                    <Modal btnUse={
+                      <svg className="icon-btn"  viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                    }>
                       <div className="modal-cent-confirmation">
                         <h2>Etes vous sur de vouloir supprimer l'utilisateur {user.firstName} {user.name} ? </h2>
                         <BtnPrimary onClick={handleDeleteGame} id={user.id} title="Confirmer la suppression"/>
                       </div>
                     </Modal>
-                   
+                    </div>
                   </td>
                 </tr>
               ))}
@@ -200,13 +206,18 @@ const  DashbordPage = ()  => {
                   <td>{game.noteStat?._avg.note}</td>
                   <td>{game.noteStat?._count.note}</td>
                   <td className="action-td">
-                    <BtnPrimary onClick={handleUpdateGame} id={game.id} title="Modifier le jeux"/>
-                    <Modal btnUse={<BtnPrimary title={'Supprimer le jeu'} />}>
+                  <div className="icon-btn-cont">
+                  <svg className="icon-btn" onClick={handleUpdateGame}  id={game.id}  viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.8536 1.14645C11.6583 0.951184 11.3417 0.951184 11.1465 1.14645L3.71455 8.57836C3.62459 8.66832 3.55263 8.77461 3.50251 8.89155L2.04044 12.303C1.9599 12.491 2.00189 12.709 2.14646 12.8536C2.29103 12.9981 2.50905 13.0401 2.69697 12.9596L6.10847 11.4975C6.2254 11.4474 6.3317 11.3754 6.42166 11.2855L13.8536 3.85355C14.0488 3.65829 14.0488 3.34171 13.8536 3.14645L11.8536 1.14645ZM4.42166 9.28547L11.5 2.20711L12.7929 3.5L5.71455 10.5784L4.21924 11.2192L3.78081 10.7808L4.42166 9.28547Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                    {/* <BtnPrimary onClick={handleUpdateGame} id={game.id} title="Modifier le jeux"/> */}
+                    <Modal btnUse={
+                      <svg className="icon-btn"  viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                    }>
                       <div className="modal-cent-confirmation">
                         <h2>Etes vous sur de vouloir supprimer le jeu {game.name} ? </h2>
                         <BtnPrimary onClick={handleDeleteGame} id={game.id} title="Confirmer la suppression"/>
                       </div>
                     </Modal>
+                    </div>
                   </td>
                  
                 </tr>
